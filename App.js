862adcc98b8ae909db5a2fc0a29069a1ca3fd8f7
bhuvayatin <59174457+bhuvayatin@users.noreply.github.com>
2020-10-30 11:53:39 +0530

    create Rootstack and splashScreen
    And Also cjange in App.js & maindrawer.js. in mainDrawer.js remove the LoginScreen and signUpscreen

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 
 // import component
 import Drawer from './NavigationBar/MainDrawer';
-
+import Rootstack from './Screen/RootStack';
 
 
 export default function App() {
   return (
     <NavigationContainer>
-     <Drawer/>
+     {/* <Drawer/> */}
+     <Rootstack/>
     </NavigationContainer>
   );
 }
--- NavigationBar/MainDrawer.js
+++ NavigationBar/MainDrawer.js
@@ -5,8 +5,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../Screen/HomeScreen';
 import ProfileScreen from '../Screen/ProfileScreen';
 import {DrawerContent}  from './DrawerIteam';
-import SignUpScreen from '../Screen/SignUpScreen';
-import SignInScreen from '../Screen/SignInScreen';
+
+
 import NotificationsScreen from '../Screen/NotificationsScreen';
 
 
@@ -20,12 +20,11 @@ function MyDrawer() {
   return (
     
      
-    <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>} initialRouteName="SignInScreen">
+    <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>} >
       <Drawer.Screen name="HomeScreen" component={HomeScreen}/>
       <Drawer.Screen name="NotificationsScreen" component={NotificationsScreen}/>
       <Drawer.Screen name="ProfileScreen" component={ProfileScreen} />
-      <Drawer.Screen name="SignUpScreen" component={SignUpScreen} />
-      <Drawer.Screen name="SignInScreen" component={SignInScreen} />
+      
     
       </Drawer.Navigator> 
   
